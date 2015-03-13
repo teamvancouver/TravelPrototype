e8bb50cdb76551a0906504d9a9a3310a0fb47e7d
teamvancouver <teamvancouver@outlook.de>
2015-03-13 10:32:52 +0100

    Hotel Segment in Model

--- TravelPrototype/Models/ItineraryModels.cs
+++ TravelPrototype/Models/ItineraryModels.cs
@@ -9,10 +9,58 @@ namespace TravelPrototype.Models
     public class ItineraryModel
     {
         public int ID { get; set; }
+        public List<SegmentModel> segments { get; set; }
     }
 
     public class ItineraryDBContext : DbContext
     {
         public DbSet<ItineraryModel> Itineraries { get; set; }
+    }
+
+    //Segments 
+    public class SegmentModel
+    {
+        public int id { get; set; }
+        public int member_id { get; set; }
+        public string type { get; set; }
+        public string status { get; set; }
+        public string hotel_name { get; set; }
+        public string normalized_hotel_chain { get; set; }
+        public string phone { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string address1 { get; set; }
+        public string address2 { get; set; }
+        public string city_name { get; set; }
+        public string admin_code { get; set; }
+        //public enum country country { get; set; }
+        public string zip { get; set; }
+        public string lat { get; set; }
+        public string lon { get; set; }
+        public DateTime checkin_date { get; set; }
+        //public enum time_zone checkin_time_zone { get; set; }
+        public DateTime checkout_date { get; set; }
+        //public enum time_zone checkout_time_zone { get; set; }
+        public string time_zone_id { get; set; }
+        public float price { get; set; }
+        //public enum Currency currency { get; set; }
+        public int number_of_rooms { get; set; }
+        public string confirmation_no { get; set; }
+        public string support_phone { get; set; }
+        public Uri support_url { get; set; }
+        public string booking_agent { get; set; }
+        public string room_type { get; set; }
+        public string room_description { get; set; }
+        public string rate_description { get; set; }
+        public string cancellation_policy { get; set; }
+        public Uri icon_url  { get; set; }
+        public Uri image_url { get; set; }
+        public string source { get; set; }
+        public string harvested { get; set; }
+        public string hotel_notes { get; set; }
+        public DateTime created { get; set; }
+        public DateTime updated { get; set; }
+        public string metadata { get; set; }
+
     }
 }
